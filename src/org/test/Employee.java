f0f5a538bd3d2b286ebbce09ad7af5ccd615d2ca
umathirukumar <sadhasivamuma@gmail.com>
2019-05-28 12:51:11 +0530

    adding program

---
+++ src/org/test/Employee.java
@@ -0,0 +1,26 @@
+package org.test;
+
+public class Employee {
+	private int id;
+	private  String name;
+	private long phono;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getPhono() {
+		return phono;
+	}
+	public void setPhono(long phono) {
+		this.phono = phono;
+	}
+
+}
